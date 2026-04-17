0e33848966c1d7c09b698ce6346318ead37d167a
Thiago Medeiros <thiagojabm@gmail.com>
2026-04-17 12:28:46 -0300

    testing

--- basics/test.py
+++ basics/test.py
@@ -1,2 +1,16 @@
-nums = [1,2,4,6]
-print(nums[2:2])
+def end_other(a, b):
+    a = a.lower()
+    b = b.lower()
+    
+    maxS = max(a, b)
+    minS = min(a, b)
+    
+    sz = len(minS)
+    
+    print(maxS[-sz:], minS)
+    if maxS[-sz:] == minS:
+        return True
+    else:
+        return False
+  
+end_other('Hiabc', 'bc')
